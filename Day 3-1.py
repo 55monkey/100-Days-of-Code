4ec9640201de5442c2fb2850cf11ba74e77a9a4d
55monkey <taningreenn@protonmail.com>
2022-06-21 11:12:40 +0100

    Nesteed Conditions, Exercise Odd and Even Numbers

---
+++ Day 3-1.py
@@ -0,0 +1,36 @@
+#Conditions
+print("Welcome to the Rollercoaster!")
+height = int(input("What is your height in cm?"))
+
+if height >= 120:
+  print("You can ride the rollercoaster")
+else:
+  print("Sorry youare unable to ride the rollercoaster")
+
+
+#Nesteed Conditions
+print("Welcome to the Rollercoaster!")
+height = int(input("What is your height in cm?"))
+
+if height >= 120:
+  print("You can ride the rollercoaster")
+  age = int(input("What is your age?"))
+  if age < 12:
+    print("Please pay $5.")
+  elif age <= 18:
+    print("Please pay $7.")
+  else:
+    print("Please pay $12.")
+else:
+  print("Sorry youare unable to ride the rollercoaster")
+
+
+#Exercise Odd and Even Numbers
+number = int(input("Which number do you want to check? "))
+
+#If the number can be divided by 2 with 0 remainder.
+if number % 2 == 0:
+  print("This is an even number.")
+#Otherwise (number cannot be divided by 2 with 0 remainder).
+else:
+  print("This is an odd number.")
