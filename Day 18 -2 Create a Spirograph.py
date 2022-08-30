c9e5a1894b427b2b34d245bab41e06764b1001c9
55monkey <taningreenn@protonmail.com>
2022-08-30 12:31:42 +0100

    Spirograph out of cirlces

---
+++ Day 18 -2 Create a Spirograph.py
@@ -0,0 +1,9 @@
+########### Challenge 5 - Spirograph ########
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+draw_spirograph(5)
